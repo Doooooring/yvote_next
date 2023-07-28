c6e0f969828f955e734b8e4a3ecffe5dbe293bdf
kang seok <sekeipost@naver.com>
2023-07-28 21:26:38 +0900

    fix the api response type

--- repositories/keywords/index.ts
+++ repositories/keywords/index.ts
@@ -4,6 +4,13 @@ import { HOST_URL } from '@url';
 import { category, KeywordOnDetail, KeywordToView } from '@utils/interface/keywords';
 import { Preview } from '@utils/interface/news';
 
+interface Response<T> {
+  data: {
+    success: boolean;
+    result: T;
+  };
+}
+
 export interface otherObject {
   _id: category;
   keywords: Array<KeywordToView>;
@@ -14,6 +21,10 @@ export interface getKeywordsResponse {
   other: Array<otherObject>;
 }
 
+export interface getKeywordsByCategoryResponse {
+  keywords: KeywordToView[];
+}
+
 export interface getKeywordDetailResponse {
   keyword: KeywordOnDetail;
   previews: Array<Preview>;
@@ -21,29 +32,56 @@ export interface getKeywordDetailResponse {
 
 class KeywordsRepository {
   async getKeywords() {
-    const response = await axios.get(`${HOST_URL}/keywords`);
-    const keywords: getKeywordsResponse = response.data;
-    return keywords;
+    try {
+      const response: Response<getKeywordsResponse> = await axios.get(`${HOST_URL}/keywords`);
+      const keywords = response.data.result;
+      return keywords;
+    } catch (e) {
+      console.log(e);
+      return {
+        recent: [],
+        other: [],
+      };
+    }
   }
 
   async getKeywordList() {
-    const response = await axios.get(`${HOST_URL}/keywords/keyword`);
-    const keylist: string[] = response.data;
-    return keylist;
+    try {
+      const response: Response<{ keywords: string[] }> = await axios.get(
+        `${HOST_URL}/keywords/keyword`,
+      );
+      const keylist: string[] = response.data.result.keywords;
+      return keylist;
+    } catch (e) {
+      console.log(e);
+      return [];
+    }
   }
 
-  async getKeywordsWithCategory() {
-    const response = await axios.get(`${HOST_URL}/keywords`);
+  async getKeywordsByCategory(category: category, page: number) {
+    try {
+      const response: Response<getKeywordsByCategoryResponse> = await axios.get(
+        `${HOST_URL}/keywords/${category}?page=${page}`,
+      );
 
-    return 0;
+      return response.data.result.keywords;
+    } catch (e) {
+      console.log(e);
+      return [];
+    }
   }
 
   async getKeywordDetail(keyword: string, curNum: number) {
-    const response = await axios.get(
-      `${HOST_URL}/keywords/detail?keyName=${keyword}&curNum=${curNum}`,
-    );
-    const keywordDetail: getKeywordDetailResponse = response.data;
-    return keywordDetail;
+    try {
+      const response: Response<getKeywordDetailResponse> = await axios.get(
+        `${HOST_URL}/keywords/detail?keyword=${keyword}&page=${curNum}`,
+      );
+      const keywordDetail = response.data.result;
+      return keywordDetail;
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
   }
 }
 
--- repositories/news/index.ts
+++ repositories/news/index.ts
@@ -48,87 +48,105 @@ interface VoteResponse {
 
 class NewsRepository {
   async getPreviews(curNum: number, keyword: string | null = null) {
-    // const response = await axios.get(`${HOST_URL}/news/preview?page=${curNum}&keyword=${keyword}`);
-    // const data: Array<Preview> = response.data;
-    // return data;
+    try {
+      const response: Response<{ news: Array<Preview> }> = await axios.get(
+        `${HOST_URL}/news/preview?page=${curNum}&keyword=${keyword}`,
+      );
+      const data = response.data;
+      return data.result.news;
+    } catch (e) {
+      console.log(e);
+      return [];
+    }
 
-    return [
-      {
-        _id: '1',
-        order: 1,
-        title: '더미데이터 제목입니다.',
-        summary:
-          '더미데이터 뉴스 요약입니다. 더미데이터 뉴스 요약입니다. 더미데이터 뉴스 요약입니다.',
-        keywords: ['키워드1', '키워드2', '키워드3'],
-        state: true,
-      },
-      {
-        _id: '2',
-        order: 2,
-        title: '더미데이터 제목입니다.',
-        summary:
-          '더미데이터 뉴스 요약입니다. 더미데이터 뉴스 요약입니다. 더미데이터 뉴스 요약입니다.',
-        keywords: ['키워드1', '키워드2', '키워드3'],
-        state: true,
-      },
-      {
-        _id: '2',
-        order: 3,
-        title: '더미데이터 제목입니다.',
-        summary:
-          '더미데이터 뉴스 요약입니다. 더미데이터 뉴스 요약입니다. 더미데이터 뉴스 요약입니다.',
-        keywords: ['키워드1', '키워드2', '키워드3'],
-        state: true,
-      },
-    ];
+    // return [
+    //   {
+    //     _id: '1',
+    //     order: 1,
+    //     title: '더미데이터 제목입니다.',
+    //     summary:
+    //       '더미데이터 뉴스 요약입니다. 더미데이터 뉴스 요약입니다. 더미데이터 뉴스 요약입니다.',
+    //     keywords: ['키워드1', '키워드2', '키워드3'],
+    //     state: true,
+    //   },
+    //   {
+    //     _id: '2',
+    //     order: 2,
+    //     title: '더미데이터 제목입니다.',
+    //     summary:
+    //       '더미데이터 뉴스 요약입니다. 더미데이터 뉴스 요약입니다. 더미데이터 뉴스 요약입니다.',
+    //     keywords: ['키워드1', '키워드2', '키워드3'],
+    //     state: true,
+    //   },
+    //   {
+    //     _id: '2',
+    //     order: 3,
+    //     title: '더미데이터 제목입니다.',
+    //     summary:
+    //       '더미데이터 뉴스 요약입니다. 더미데이터 뉴스 요약입니다. 더미데이터 뉴스 요약입니다.',
+    //     keywords: ['키워드1', '키워드2', '키워드3'],
+    //     state: true,
+    //   },
+    // ];
   }
 
-  async getNewsContent(id: Preview['_id']): Promise<getNewsContentResponse> {
-    // const token = localStorage.getItem('yVote');
-    // const response = await axios.get(`${HOST_URL}/news/detail?id=${id}`, {
-    //   headers: {
-    //     authorization: token,
+  async getNewsContent(id: Preview['_id']) {
+    try {
+      const token = localStorage.getItem('yVote');
+      const response: Response<getNewsContentResponse> = await axios.get(
+        `${HOST_URL}/news/detail?id=${id}`,
+        {
+          headers: {
+            authorization: token,
+          },
+        },
+      );
+      const data = response.data;
+      if (!data.success) Error('api error');
+
+      return data.result;
+    } catch (e) {
+      return {
+        response: 'none',
+        news: null,
+      };
+    }
+    // return {
+    //   response: 'none',
+    //   news: {
+    //     _id: '1',
+    //     order: 1,
+    //     title: '더미데이터 제목입니다',
+    //     summary:
+    //       '더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.$ 더미데이터 내용입니다. 더미데이터 내용입니다. 더미데이터 내용입니다.',
+    //     keywords: ['키워드1', '키워드2', '키워드3'],
+    //     state: true,
+    //     timeline: [
+    //       {
+    //         date: '2017.08.12',
+    //         title: '더미데이터 입니다.',
+    //       },
+    //       {
+    //         date: '2017.08.12',
+    //         title: '더미데이터 입니다.',
+    //       },
+    //       {
+    //         date: '2017.08.12',
+    //         title: '더미데이터 입니다.',
+    //       },
+    //     ],
+    //     opinions: {
+    //       left: '왼쪽 의견 입니다.',
+    //       right: '오른쪽 의견입니다.',
+    //     },
+    //     comments: [commentType.감시자, commentType.개혁가, commentType.관찰자, commentType.민주당],
+    //     votes: {
+    //       left: 1,
+    //       right: 1,
+    //       none: 1,
+    //     },
     //   },
-    // });
-    // const data: getNewsContentResponse = response.data;
-    // console.log(data);
-    // return data;
-    return {
-      response: 'none',
-      news: {
-        _id: '1',
-        order: 1,
-        title: '더미데이터 제목입니다',
-        summary:
-          '더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.더미데이터 내용입니다.$ 더미데이터 내용입니다. 더미데이터 내용입니다. 더미데이터 내용입니다.',
-        keywords: ['키워드1', '키워드2', '키워드3'],
-        state: true,
-        timeline: [
-          {
-            date: '2017.08.12',
-            title: '더미데이터 입니다.',
-          },
-          {
-            date: '2017.08.12',
-            title: '더미데이터 입니다.',
-          },
-          {
-            date: '2017.08.12',
-            title: '더미데이터 입니다.',
-          },
-        ],
-        opinions: {
-          left: '왼쪽 의견 입니다.',
-          right: '오른쪽 의견입니다.',
-        },
-        comments: [commentType.감시자, commentType.개혁가, commentType.관찰자, commentType.민주당],
-        votes: {
-          left: 1,
-          right: 1,
-          none: 1,
-        },
-      },
-    };
+    // };
   }
 
   async getNewsComment(
