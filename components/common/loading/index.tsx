283a370f728ec4c744e211489bcba132769c0834
8gundogan <mjyourface97@gmail.com>
2024-04-06 15:22:18 +0900

    I don't know why these are on my 'changes' may becuase I accidentally typed git pull origin master? Let me know if there are any problems.

--- components/common/loading/index.tsx
+++ components/common/loading/index.tsx
@@ -1,13 +1,56 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { LoadingCommonProps } from './loadingCommon.type';
 
-export default function Loading({state} : {state : boolean}) {
-    return <Wrapper state={state}></Wrapper>
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+/**
+ * 로딩 상태 공통 컴포넌트
+ */
+export default function LoadingCommon({ comment }: LoadingCommonProps) {
+  return (
+    <Wrapper>
+      <FontAwesomeIcon className="spinner" icon={faSpinner} width={16} />
+      <p className="comment">{comment}</p>
+    </Wrapper>
+  );
 }
 
-interface WrapperProps {
-    state : boolean;
-}
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  font-size: 1rem;
+  padding: 1rem 0;
 
-const Wrapper = styled.div<WrapperProps>`
+  .spinner {
+    animation: spin 2.5s linear infinite;
 
-`
+    @keyframes spin {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  }
+
+  .comment {
+    animation: blink 0.75s ease-in-out infinite;
+
+    @keyframes blink {
+      0% {
+        opacity: 1;
+      }
+      100% {
+        opacity: 0.2;
+      }
+    }
+  }
+`;
