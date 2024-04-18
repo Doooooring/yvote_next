11146759d466583c2551ee6e03f6d50a46d986a7
kang seok <sekeipost@naver.com>
2024-04-18 12:47:22 +0900

    delete the unused module

--- utils/hook/useNumberIncreasing.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from 'react';
-
-import { useAnimation } from './useAnimation';
-
-export function useNumberIncreasing(num: number): number {
-  const curNum = useAnimation(num, 40);
-
-  return curNum;
-}
