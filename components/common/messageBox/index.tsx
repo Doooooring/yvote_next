85eb37d8d66949be5a2627997b8d08444e864066
doring <2017132021@yonsei.ac.kr>
2024-10-17 17:46:04 +0900

    fix message responsive design

--- components/common/messageBox/index.tsx
+++ components/common/messageBox/index.tsx
@@ -25,9 +25,25 @@ export function NegativeMessageBox({ children, style }: MessageBoxProps) {
 
 const Wrapper = styled(CommonLayoutBox)`
   box-sizing: content-box;
-  padding: 0.5rem 2rem;
-  min-width: 200px;
-  max-width: 90%;
+  padding: 0.5rem 1rem;
+  min-width: 300px;
+
+  p {
+    white-space: nowrap;
+    font-weight: 500;
+
+    font-size: 16px;
+  }
+
+  @media screen and (max-width: 550px) {
+    min-width: 200px;
+    padding: 0.4rem 0.75rem;
+    p {
+      font-weight: 500;
+
+      font-size: 13px;
+    }
+  }
 
   position: relative;
   z-index: 1;
@@ -37,10 +53,6 @@ const PositiveWrapper = styled(Wrapper)`
   color: ${({ theme }) => RGBA(theme.colors.yvote02, 1)};
   border-color: ${({ theme }) => RGBA(theme.colors.yvote02, 1)};
   background-color: white;
-  p {
-    font-weight: 500;
-    font-size: 16px;
-  }
 `;
 
 const NegativeWrapper = styled(Wrapper)``;
