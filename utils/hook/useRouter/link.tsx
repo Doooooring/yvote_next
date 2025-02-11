ee92adf8db21b201f4093b2390a161e5ec5642d8
kang seok <sekeipost@naver.com>
2025-02-11 23:56:48 +0900

    add custom Link

--- utils/hook/useRouter/link.tsx
+++ utils/hook/useRouter/link.tsx
@@ -1,1 +1,25 @@
-import Link
+import LinkOrg, { LinkProps as OrgLinkProps } from 'next/link';
+import { MouseEvent, ReactNode, useCallback } from 'react';
+import { useRouter } from './useRouter';
+
+interface LinkProps extends OrgLinkProps {
+  children: ReactNode;
+}
+
+export default function Link({ href, children, ...others }: LinkProps) {
+  const { router } = useRouter();
+
+  const defaultClickHandler = useCallback((e: MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    if (e.altKey) {
+      window.open(href.toString(), '_blank');
+    }
+    router.push(href);
+  }, []);
+
+  return (
+    <LinkOrg href={href} {...others}>
+      {children}
+    </LinkOrg>
+  );
+}
